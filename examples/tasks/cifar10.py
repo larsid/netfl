d288d0a323d4b6807112045e74ce918f3631fed6
Sesaque Cruz <cruz.sesaque@gmail.com>
2025-03-22 20:11:30 -0300

    chore: add task examples

---
+++ examples/tasks/cifar10.py
@@ -0,0 +1,84 @@
+from keras import layers, models
+from flwr.server.strategy import Strategy, FedAvg
+from flwr.common import Metrics
+
+from fogfl.core.task import Dataset, Task, TrainConfig, DatasetConfig
+
+
+class Cifar10(Task):
+    def __init__(self) -> None:
+        train_config = TrainConfig(
+            batch_size=32,
+            epochs=1,
+            fraction_evaluate=1.0,
+            fraction_fit=1.0,
+            learning_rate=0.001,
+            min_available=2,
+            num_clients=2,
+            num_rounds=3,
+            seed=42,
+            shuffle=True,
+            test_size=0.2,
+            verbose="2",
+        )
+        dataset_config = DatasetConfig(
+            dataset_name="uoft-cs/cifar10",
+            item_name="img",
+            label_name="label",
+        )
+        super().__init__(train_config, dataset_config)
+
+    def client_dataset(self, client_id: int) -> Dataset:
+        dataset = self._dataset_partition(client_id)
+        normalized_dataset = Dataset(
+            x_train=(dataset.x_train / 255.0),
+            x_test=(dataset.x_test / 255.0),
+            y_train=dataset.y_train,
+            y_test=dataset.y_test,
+        )
+        return normalized_dataset
+
+    def model(self) -> models.Model:
+        model = models.Sequential(
+            [
+                layers.Input(shape=(32, 32, 3)),
+
+                layers.Conv2D(32, kernel_size=(3, 3), activation="relu", padding="same"),
+                layers.BatchNormalization(),
+                layers.MaxPooling2D(pool_size=(2, 2)),
+
+                layers.Conv2D(64, kernel_size=(3, 3), activation="relu", padding="same"),
+                layers.BatchNormalization(),
+                layers.MaxPooling2D(pool_size=(2, 2)),
+
+                layers.Conv2D(128, kernel_size=(3, 3), activation="relu", padding="same"),
+                layers.BatchNormalization(),
+                layers.MaxPooling2D(pool_size=(2, 2)),
+
+                layers.Flatten(),
+
+                layers.Dense(512, activation="relu"),
+                layers.Dropout(0.5),
+
+                layers.Dense(10, activation="softmax"),
+            ]
+        )
+
+        model.compile(
+            optimizer="adam",
+            loss="sparse_categorical_crossentropy",
+            metrics=["accuracy"],
+        )
+
+        return model
+
+    def aggregation_strategy(self) -> Strategy:
+        return self._aggregation_strategy_factory(FedAvg)
+    
+    def aggregation_evaluate_metrics(self, metrics: list[tuple[int, Metrics]]) -> Metrics:
+        print(metrics)
+        return {}
+
+
+class MainTask(Cifar10):
+    pass
