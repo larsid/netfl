2a30868a1b5bd5ce39b802c8cb1211f697521633
Sesaque Cruz <cruz.sesaque@gmail.com>
2025-05-18 01:39:06 -0300

    refactor: remove unused params from netfl experiment api

--- netfl/core/experiment.py
+++ netfl/core/experiment.py
@@ -12,7 +12,6 @@ class NetflExperiment(FogbedDistributedExperiment):
 	def __init__(self, 
 		main_task: type[Task],
 		dimage: str = "netfl/netfl",
-		server_port: int = 9191,
 		controller_ip: str | None = None,
     	controller_port: int = 6633,
 		max_cpu: float = 1,
@@ -25,8 +24,8 @@ class NetflExperiment(FogbedDistributedExperiment):
 		self._task = main_task()
 		self._task_dir = get_task_dir(self._task)
 		self._dimage = dimage
-		self._server_port = server_port
 		self._server: Container | None = None
+		self._server_port: int | None = None
 		self._devices: list[Container] = []
 
 	@property
@@ -36,41 +35,35 @@ class NetflExperiment(FogbedDistributedExperiment):
 	def create_server(
 		self, 
 		ip: str | None = None,
-		port: int | None = None,
+		port: int = 9191,
 		resources: HardwareResources = Resources.SMALL,
 		link_params: dict[str, Any] = {},
-    	**params: Any,
 	) -> Container:
 		if self._server is not None:
 			raise RuntimeError("The experiment already has a server.")
-		
-		if port is not None:
-			self._server_port = port
 		
 		self._server = Container(
 			name="server", 
 			ip=ip,
 			dimage=self._dimage,
-			dcmd=f"python -u run.py --type=server --server_port={self._server_port}",
+			dcmd=f"python -u run.py --type=server --server_port={port}",
 			environment={EXPERIMENT_ENV_VAR: self._experiment_id},
-			port_bindings={self._server_port:self._server_port},
+			port_bindings={port:port},
 			volumes=[
 				f"{self._task_dir}/task.py:/app/task.py",
 				f"{self._task_dir}/logs:/app/logs"
 			],
 			resources=resources,
 			link_params=link_params,
-			params=params,
 		)
+		self._server_port = port
 
 		return self._server
 
 	def create_device(
 		self,
-		ip: str | None = None,
 		resources: HardwareResources = Resources.SMALL,
 		link_params: dict[str, Any] = {},
-    	**params: Any,
 	) -> Container:
 		if self._server is None:
 			raise RuntimeError("The server must be created before creating devices.")
@@ -81,13 +74,11 @@ class NetflExperiment(FogbedDistributedExperiment):
 		device_id = len(self._devices)
 		device = Container(
 			name=f"device_{device_id}",
-			ip=ip,
 			dimage=self._dimage,
 			dcmd=f"python -u run.py --type=client --client_id={device_id} --server_address={self._server.ip} --server_port={self._server_port}",
 			environment={EXPERIMENT_ENV_VAR: self._experiment_id},
 			resources=resources,
 			link_params=link_params,
-			params=params,
 		)
 		self._devices.append(device)
 
