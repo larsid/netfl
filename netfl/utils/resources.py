29f46f4d7241b3c2d7bdbb504b93927d829f4e58
Sesaque Cruz <cruz.sesaque@gmail.com>
2025-07-20 03:33:01 -0300

    feat: add function to calculate compute units

--- netfl/utils/resources.py
+++ netfl/utils/resources.py
@@ -11,3 +11,10 @@ class LinkResources:
 	@property
 	def params(self) -> dict[str, Any]:
 		return {k: v for k, v in vars(self).items() if v is not None}
+
+
+def calculate_compute_units(clock_host_ghz: float, clock_device_ghz: float) -> float:
+    if clock_host_ghz <= 0 or clock_device_ghz <= 0:
+        raise ValueError("Clocks must be greater than zero.")
+
+    return round(clock_device_ghz / clock_host_ghz, 3)
