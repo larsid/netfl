16c02083c20997b8af4799ddf522dc9ddca8e925
Sesaque Cruz <cruz.sesaque@gmail.com>
2025-09-15 04:09:23 -0300

    feat: use padding same in conv2d layers

--- netfl/core/models.py
+++ netfl/core/models.py
@@ -8,11 +8,11 @@ def cnn3(
 	) -> models.Model:
 	model = models.Sequential([
 		layers.Input(shape=input_shape),
-
-		layers.Conv2D(32, kernel_size=(5, 5), activation="relu", padding="valid"),
+		
+		layers.Conv2D(32, kernel_size=(5, 5), activation="relu", padding="same"),
 		layers.MaxPooling2D(pool_size=(2, 2)),
 
-		layers.Conv2D(64, kernel_size=(5, 5), activation="relu", padding="valid"),
+		layers.Conv2D(64, kernel_size=(5, 5), activation="relu", padding="same"),
 		layers.MaxPooling2D(pool_size=(2, 2)),
 
 		layers.Flatten(),
