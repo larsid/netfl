18a05aeb33b163f1a3cb55a595a9b654b4c137c1
Sesaque Cruz <cruz.sesaque@gmail.com>
2025-09-16 01:56:06 -0300

    refactor: update cnn3 model

--- netfl/core/models.py
+++ netfl/core/models.py
@@ -9,10 +9,10 @@ def cnn3(
 	model = models.Sequential([
 		layers.Input(shape=input_shape),
 
-		layers.Conv2D(32, kernel_size=(5, 5), activation="relu", padding="valid"),
+		layers.Conv2D(32, kernel_size=(5, 5), activation="relu"),
 		layers.MaxPooling2D(pool_size=(2, 2)),
 
-		layers.Conv2D(64, kernel_size=(5, 5), activation="relu", padding="valid"),
+		layers.Conv2D(64, kernel_size=(5, 5), activation="relu"),
 		layers.MaxPooling2D(pool_size=(2, 2)),
 
 		layers.Flatten(),
