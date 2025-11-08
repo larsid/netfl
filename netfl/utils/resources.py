d23f2606d969147922f27881bdf14ba5a5d12e57
Sesaque Cruz <cruz.sesaque@gmail.com>
2025-11-08 00:44:07 -0300

    fix: increase compute unit precision from 3 to 6 for improved accuracy

--- netfl/utils/resources.py
+++ netfl/utils/resources.py
@@ -6,7 +6,7 @@ from fogbed.resources.protocols import ResourceModel
 from fogbed import CloudResourceModel, FogResourceModel, EdgeResourceModel
 
 
-COMPUTE_UNIT_PRECISION = 3
+COMPUTE_UNIT_PRECISION = 6
 COMPUTE_UNIT_ERROR = 1 / 10 ** (COMPUTE_UNIT_PRECISION + 1)
 
 
