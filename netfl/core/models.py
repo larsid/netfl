0d29ae5e625d848762af687e4f5273c814fee53a
Sesaque Cruz <cruz.sesaque@gmail.com>
2025-12-06 19:31:41 -0300

    refactor: enhance cnn3 model with initializer and data augmentation layers

--- netfl/core/models.py
+++ netfl/core/models.py
@@ -1,4 +1,9 @@
-from keras import layers, models, optimizers
+from keras import layers, models, initializers, optimizers, losses
+
+
+DEFAULT_INITIALIZER = initializers.VarianceScaling(
+    scale=1.0 / 3.0, mode="fan_in", distribution="uniform"
+)
 
 
 def cnn3(
@@ -9,19 +14,21 @@ def cnn3(
     model = models.Sequential(
         [
             layers.Input(shape=input_shape),
-            layers.Conv2D(32, kernel_size=(5, 5), activation="relu"),
-            layers.MaxPooling2D(pool_size=(2, 2)),
-            layers.Conv2D(64, kernel_size=(5, 5), activation="relu"),
-            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.RandomFlip("horizontal"),
+            layers.RandomTranslation(0.1, 0.1),
+            layers.Conv2D(32, (5, 5), activation="relu", kernel_initializer=DEFAULT_INITIALIZER),  # type: ignore[arg-type]
+            layers.MaxPooling2D((2, 2)),
+            layers.Conv2D(64, (5, 5), activation="relu", kernel_initializer=DEFAULT_INITIALIZER),  # type: ignore[arg-type]
+            layers.MaxPooling2D((2, 2)),
             layers.Flatten(),
-            layers.Dense(512, activation="relu"),
-            layers.Dense(output_classes, activation="softmax"),
+            layers.Dense(512, activation="relu", kernel_initializer=DEFAULT_INITIALIZER),  # type: ignore[arg-type]
+            layers.Dense(output_classes, kernel_initializer=DEFAULT_INITIALIZER),  # type: ignore[arg-type]
         ]
     )
 
     model.compile(
         optimizer=optimizer,  # type: ignore[arg-type]
-        loss="sparse_categorical_crossentropy",
+        loss=losses.SparseCategoricalCrossentropy(from_logits=True),
         metrics=["accuracy"],
     )
 
